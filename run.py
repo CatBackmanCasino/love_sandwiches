0b27a92ab93ca703fb89516c4c8c839a17421ca5
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-04-05 16:26:34 +0000

    add validation checking if user input has six numbers

--- run.py
+++ run.py
@@ -23,5 +23,20 @@ def get_sales_data():
     data_str = input("Enter sales data here: ")
     print(f"The data provided is {data_str}")
 
-    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 numbers required.\n You provided {len(values)}"
+                )
+    except ValueError as e:
+        print(f"Invalid Data: {e}. Please try again. \n") 
+
+    print(values)
+
+
 get_sales_data()
